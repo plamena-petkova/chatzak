b3bdc38e3ad4119702fc71877b550df49825f433
Plamena Petkova <pl.petkova@gmail.com>
2023-10-28 00:42:44 +0300

    Modification in the data to show the badge of the online user

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,27 +1,38 @@
-import { Avatar, Badge, Tab} from "@mui/joy";
+import { Avatar, Badge, Tab } from "@mui/joy";
 import { useSelector } from "react-redux";
 
-function ContactCard({contact}) {
-
+function ContactCard({ contact }) {
   const onlineUsers = useSelector((state) => state.auth.onlineUsers);
-  const online = onlineUsers.map((user) => user.userId);
-  const onlineUser = online.includes(contact._id)
-
+  const onlineUser = Object.values(onlineUsers).includes(contact._id);
 
   return (
-  
-      <Tab key={contact._id} sx={{ minWidth: 270 }}>
-        {onlineUser ?  <Badge><Avatar key={contact._id} src={`${contact?.avatarImg}`} sx={{ mr:1 }}>
+    <Tab key={contact._id} sx={{ minWidth: 270 }}>
+      {onlineUser ? (
+        <Badge
+        badgeInset="14%"
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          size="sm"
+        >
+          <Avatar
+            key={contact._id}
+            src={`${contact?.avatarImg}`}
+            sx={{ mr: 1 }}
+          >
+            {contact.avatarImg ? contact.avatar : contact.names[0]}
+          </Avatar>
+        </Badge>
+      ) : (
+        <Avatar key={contact._id} src={`${contact?.avatarImg}`} sx={{ mr: 1 }}>
           {contact.avatarImg ? contact.avatar : contact.names[0]}
-        </Avatar></Badge> : <Avatar key={contact._id} src={`${contact?.avatarImg}`} sx={{ mr:1 }}>
-          {contact.avatarImg ? contact.avatar : contact.names[0]}
-        </Avatar>}
-       
-        {contact.names}
-      </Tab>
+        </Avatar>
+      )}
 
+      {contact.names}
+    </Tab>
   );
 }
 
 export default ContactCard;
-
