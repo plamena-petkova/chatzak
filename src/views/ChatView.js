67415a238b5b78e205061f4355567cdb9c5597d9
Plamena Petkova <pl.petkova@gmail.com>
2024-09-08 16:32:24 +0300

    Redesign of chatView

--- src/views/ChatView.js
+++ src/views/ChatView.js
@@ -1,9 +1,15 @@
-import { Box } from "@mui/joy";
+import { TabList, TabPanel, Tabs, Tab } from "@mui/joy";
 import ChatComponent from "../components/ChatComponent";
 import Header from "../components/Header";
 import SnackbarComponent from "../components/SnackbarComponent";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import PersonPinIcon from "@mui/icons-material/PersonPin";
+import ListItemDecorator from '@mui/joy/ListItemDecorator';
+import ContactsIcon from '@mui/icons-material/Contacts';
+
+
 
 function ChatView() {
   const newMessageIndicator = useSelector(
@@ -12,19 +18,58 @@ function ChatView() {
   const newMessageObject = Object.values(newMessageIndicator);
   const [openNewSnack, setOpenNewSnack] = useState(false);
 
-
-useEffect(() => {
-    if(newMessageObject.map(item => item.show).includes(true)) {
-        setOpenNewSnack(true);
+  useEffect(() => {
+    if (newMessageObject.map((item) => item.show).includes(true)) {
+      setOpenNewSnack(true);
     }
-
-}, [openNewSnack, newMessageObject])
-
+  }, [openNewSnack, newMessageObject]);
 
   const handleCloseSnack = () => {
     setOpenNewSnack(false);
   };
 
+
+  return (
+    <>
+    <Header />
+    <SnackbarComponent open={openNewSnack} handleClose={handleCloseSnack} />
+    <Tabs aria-label="Icon tabs" defaultValue={0}>
+    <TabList>
+      <Tab value={"chat"}>
+        <ListItemDecorator>
+          <ChatBubbleIcon />
+        </ListItemDecorator>
+        Chats
+      </Tab>
+      <Tab value={"users"}>
+        <ListItemDecorator>
+          <ContactsIcon />
+        </ListItemDecorator>
+        Contacts
+      </Tab>
+      <Tab value={"person"}>
+        <ListItemDecorator>
+          <PersonPinIcon />
+        </ListItemDecorator>
+        My Profile
+      </Tab>
+    </TabList>
+    <TabPanel value="chat">
+      
+       <ChatComponent />
+       
+      </TabPanel>
+      <TabPanel value="users">
+        Users all
+      </TabPanel>
+      <TabPanel value="person">
+        My profile should be placed here!
+      </TabPanel>
+  </Tabs>
+  </>
+  );
+
+  /*
   return (
     <Box
       sx={{
@@ -43,6 +88,7 @@ useEffect(() => {
       <ChatComponent />
     </Box>
   );
+  */
 }
 
 export default ChatView;
